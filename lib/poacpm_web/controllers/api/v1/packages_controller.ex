b6244c4af0908fa45b67e1692e7487d2c3614901
matken11235 <matken11235@gmail.com>
2018-05-20 07:17:15 +0900

    Improve elasticsearch

--- lib/poacpm_web/controllers/api/v1/packages_controller.ex
+++ lib/poacpm_web/controllers/api/v1/packages_controller.ex
@@ -1,13 +1,39 @@
 defmodule PoacpmWeb.Api.V1.PackagesController do
-  alias ExAws.Dynamo
   use PoacpmWeb, :controller
 
   def index(conn, %{"search" => word}) do
-    {_resp, tables} = Dynamo.list_tables() |> ExAws.request()
-    json(conn, tables["TableNames"])
+    # AWS ESに対して，アクセスコントロールを行う．
+    # Elixirのex_awsでAWS_ACCESS_KEY_IDとAWS_SECRET_ACCESS_KEYを用いたアクセス処理を行う
+    # Elmで，APIを実行する！ VPCは？？？
+#    endpoint = "https://search-poacpm-2gud3ols5i62ko67jhgu2e3z4a.ap-northeast-1.es.amazonaws.com/package/_search"
+#    body = "{
+#      \"suggest\": {
+#        \"my-suggestion\": {
+#          \"prefix\": \"#{word}\",
+#          \"completion\": {
+#            \"field\": \"name\"
+#          }
+#        }
+#      }
+#    }"
+#    {:ok, response} = HTTPoison.post(endpoint, body, [{"Content-Type", "application/json"}])
+#    %HTTPoison.Response{body: res, headers: _, request_url: _, status_code: 200} = response
+#    suggest = get_suggest(res)
+#    json(conn, suggest)
+    res = Poacpm.ElasticSearch.suggest(word)
+#    Poacpm.ElasticSearch.suggest(word)
+    json(conn, %{"word" => res})
   end
+  def index(conn, _), do: json(conn, PoacpmWeb.Api.ErrorView.render("404.json"))
 
-  def index(conn, _) do
-    json(conn, PoacpmWeb.Api.ErrorView.render("404.json", %{}))
+
+  defp get_suggest(res) do
+    res
+    |> Poison.Parser.parse!()
+    |> Map.fetch!("suggest")
+    |> Map.fetch!("my-suggestion")
+    |> Enum.at(0)
+    |> Map.fetch!("options")
+    |> Enum.flat_map(fn(x) -> [Map.fetch!(x, "text")] end)
   end
 end
--- lib/poacpm_web/views/api/error_view.ex
+++ lib/poacpm_web/views/api/error_view.ex
@@ -11,7 +11,7 @@ defmodule PoacpmWeb.Api.ErrorView do
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
-  def template_not_found(_template, assigns) do
-    render("500.json", assigns)
+  def template_not_found(_template, _assigns) do
+    %{errors: %{detail: "Internal server error"}}
   end
 end
