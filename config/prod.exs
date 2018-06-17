1095be7c7973594756983161279cadd8271f7eb3
matken11235 <matken11235@gmail.com>
2018-06-17 17:49:34 +0900

    Turned off debug mode in production

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :poacpm, PoacpmWeb.Endpoint,
   load_from_system_env: true,
-  debug_errors: true,
+  debug_errors: false,
   url: [host: "poac.pm", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
