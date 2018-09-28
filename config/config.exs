3826a9b05130905ae0d6bbe79449063487ef74df
Ken Matsui <matken11235@gmail.com>
2018-09-28 21:33:25 +0900

    Update config.exs (#143)

--- config/config.exs
+++ config/config.exs
@@ -12,9 +12,6 @@ config :poacpm, PoacpmWeb.Endpoint,
   render_errors: [view: PoacpmWeb.Api.ErrorView, accepts: ~w(json)],
   pubsub: [name: Poacpm.PubSub, adapter: Phoenix.PubSub.PG2]
 
-config :goth,
-  json: "/service/goth-credentials" |> File.read!
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
