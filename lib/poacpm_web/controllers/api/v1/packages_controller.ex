9613946746a46e46922f5d7b442fabc4d18eb951
matken11235 <matken11235@gmail.com>
2018-10-26 20:43:55 +0900

    Invalidate uploading files exceeding 200MB

--- lib/poacpm_web/controllers/api/v1/packages_controller.ex
+++ lib/poacpm_web/controllers/api/v1/packages_controller.ex
@@ -55,9 +55,9 @@ defmodule PoacpmWeb.Api.V1.PackagesController do
   end
 
   @api_url "https://www.googleapis.com/storage/v1/b/re.poac.pm/o/"
-  def getMd5Hash(name, version) do
+  def getMd5Hash(filename) do
     HTTPoison.start()
-    res = HTTPoison.get!(@api_url <> orgToPkg(name) <> "-" <> version <> ".tar.gz")
+    res = HTTPoison.get!(@api_url <> filename)
     case res do
       %{status_code: 200, body: body} ->
         body
@@ -66,41 +66,71 @@ defmodule PoacpmWeb.Api.V1.PackagesController do
     end
   end
 
+  def calc_file_size(path) do
+    case File.stat(path) do
+      {:ok, %{size: size}} -> size
+      {:error, reason} -> reason # handle error
+    end
+  end
+
+  @doc """
+  IF >200MB, invalid
+  Note: There is a possibility of difference depending on OS and file system.
+  Ret: ture -> valid, false -> invalid
+  """
+  def check_file_size(size) do
+    size < 200000000
+  end
+
+
+  @doc """
+  Note: This file is temporary, and Plug will remove it
+         from the directory as the request completes.
+        If we need to do anything with this file,
+         we need to do it before then.
+  Ref: https://phoenixframework.org/blog/file-uploads
+  """
   def upload(conn, %{"user" => user_params}) do
-    # Note: This file is temporary, and Plug will remove it
-    #  from the directory as the request completes.
-    # If we need to do anything with this file, we need to do it before then.
-    # https://phoenixframework.org/blog/file-uploads
-    setting = user_params["setting"]
+    file = user_params["data"]
+    file_size = file.path
+                |> calc_file_size()
+    check_ret = file_size
+                |> check_file_size()
+
+    if check_ret do
+      setting = user_params["setting"]
+                |> Poison.decode!()
+                |> Map.get("setting")
+                |> YamlElixir.read_from_string!()
+
+      token = user_params["token"]
               |> Poison.decode!()
-              |> Map.get("setting")
-              |> YamlElixir.read_from_string!()
-
-    token = user_params["token"]
-            |> Poison.decode!()
-            |> Map.get("token")
+              |> Map.get("token")
 
 
-    owners = setting["owners"]
-    name = setting["name"]
-    version = setting["version"]
-    # TODO: もし，存在していても，ownerが一致していれば，上書きができる(next version)
-    if _validate(token, owners) and !_exists(name, version) do
-      # Upload to google cloud storage
-      file = user_params["data"]
-      GCS.upload_file(file.filename, file.path)
+      owners = setting["owners"]
+      name = setting["name"]
+      version = setting["version"]
+      # TODO: もし，存在していても，ownerが一致していれば，上書きができる(next version)
+      if _validate(token, owners) and !_exists(name, version) do
+        # Upload to google cloud storage
+        GCS.upload_file(file.filename, file.path) # TODO: このfile.filenameを使用してしてしまうと，hackされる恐れが高まる
 
-      # Wait pending... (10s)
-      Process.sleep(10000)
+        # Wait pending... (10s)
+        Process.sleep(10000)
 
-      setting
-      |> Map.put("md5hash", getMd5Hash(name, version))
-      |> Firestore.create_doc()
+        setting
+        |> Map.put("object_name", file.filename)
+        |> Firestore.create_doc()
 
-      # Wait pending... (5s)
-      Process.sleep(5000)
+        # Wait pending... (5s)
+        Process.sleep(5000)
 
-      text(conn, "ok")
+        text(conn, "ok")
+      else
+        text(conn, "err")
+      end
+
     else
       text(conn, "err")
     end
