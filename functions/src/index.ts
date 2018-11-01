d6494969ea5a9a86c904dbff2a54beec73fe74a2
matken11235 <matken11235@gmail.com>
2018-11-01 17:46:00 +0900

    return version only

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -42,11 +42,20 @@ router.get("/packages/:name/:version/deps", async (req: express.Request, res: ex
     const name = req.params.name;
     const version = req.params.version;
 
+    // TODO: 存在しないパッケージを指定した場合，time outまで待つ必要があり，遅い
     const querySnapshot = await getSpecNameVersion(name, version);
     querySnapshot.forEach((doc) => {
         const packageInfo = doc.data();
-        const deps = packageInfo.deps;
+        let deps = packageInfo.deps;
         if (deps) {
+            for (let key in deps) {
+                if (deps[key]["src"] === "poac") {
+                    deps[key] = deps[key]["version"];
+                }
+                else if (deps[key]["src"] === "github") {
+                    deps[key] = deps[key]["tag"];
+                }
+            }
             res.status(200).send(deps);
         } else {
             res.status(200).send("null");
@@ -62,8 +71,16 @@ router.get("/packages/:org/:name/:version/deps", async (req: express.Request, re
     const querySnapshot = await getSpecNameVersion(name, version);
     querySnapshot.forEach((doc) => {
         const packageInfo = doc.data();
-        const deps = packageInfo.deps;
+        let deps = packageInfo.deps;
         if (deps) {
+            for (let key in deps) {
+                if (deps[key]["src"] === "poac") {
+                    deps[key] = deps[key]["version"];
+                }
+                else if (deps[key]["src"] === "github") {
+                    deps[key] = deps[key]["tag"];
+                }
+            }
             res.status(200).send(deps);
         } else {
             res.status(200).send("null");
