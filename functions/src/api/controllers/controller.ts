47d4da5493c65ea4f3e29aab24801a225efe9f64
matken11235 <matken11235@gmail.com>
2018-11-16 17:48:44 +0900

    Improve controller.ts

--- functions/src/api/controllers/controller.ts
+++ functions/src/api/controllers/controller.ts
@@ -2,22 +2,62 @@ import { Request, Response } from 'express';
 import { admin, firestore } from "../../common/firebase";
 
 
-async function getSpecNameVersion(name: string, version: string): Promise<any> {
+async function getSpecNameVersionFromFirestore(name: string, version: string): Promise<any> {
     return await firestore.collection("packages")
         .where("name", "==", name)
         .where("version", "==", version)
         .get();
 }
+
+function getQuerySnapshotHead(qs: FirebaseFirestore.QuerySnapshot)
+    : FirebaseFirestore.QueryDocumentSnapshot | null {
+    return qs.empty ? null : qs.docs[0];
+}
+
+function pickSpecNameVersion(qs: FirebaseFirestore.QuerySnapshot): object | null {
+    const doc: FirebaseFirestore.QueryDocumentSnapshot | null = getQuerySnapshotHead(qs);
+
+    if (doc === null) {
+        return null;
+    }
+
+    const packageInfo = doc.data();
+    if (packageInfo.deps) {
+        let deps = packageInfo.deps;
+        let dep = {};
+        let i: number = 0;
+        for (const key in deps) {
+            if (deps[key]["src"] === "poac") {
+                deps[key] = deps[key]["version"];
+            }
+            else if (deps[key]["src"] === "github") {
+                deps[key] = deps[key]["tag"];
+            }
+            dep[i.toString()] = {
+                "name": key,
+                "version": deps[key]
+            };
+            ++i;
+        }
+        return dep;
+    }
+    else {
+        return null;
+    }
+}
+
 async function getVersions(name: string): Promise<any> {
     return await firestore.collection("packages")
         .where("name", "==", name)
         .get();
 }
+
 async function getToken(token: string): Promise<any> {
     return await firestore.collection("tokens")
         .where(admin.firestore.FieldPath.documentId(), "==", token)
         .get();
 }
+
 async function getUser(id: string): Promise<any> {
     return await firestore.collection("users")
         .where("id", "==", id)
@@ -26,68 +66,28 @@ async function getUser(id: string): Promise<any> {
 
 class Controller {
     async deps(req: Request, res: Response): Promise<any> {
-        // Guess what, uid will NEVER be null in this
-        //  context because of the Express router.
         const name = req.params.name;
         const version = req.params.version;
 
-        // TODO: 存在しないパッケージを指定した場合，time outまで待つ必要があり，遅い
-        const querySnapshot = await getSpecNameVersion(name, version);
-        querySnapshot.forEach((doc) => {
-            const packageInfo = doc.data();
-            if (packageInfo.deps) {
-                let deps = packageInfo.deps;
-                let dep = {};
-                let i: number = 0;
-                for (const key in deps) {
-                    if (deps[key]["src"] === "poac") {
-                        deps[key] = deps[key]["version"];
-                    }
-                    else if (deps[key]["src"] === "github") {
-                        deps[key] = deps[key]["tag"];
-                    }
-                    dep[i.toString()] = {
-                        "name": key,
-                        "version": deps[key]
-                    };
-                    ++i;
-                }
-                res.status(200).send(dep);
-            } else {
-                res.status(200).send("null");
-            }
-        });
+        const querySnapshot: FirebaseFirestore.QuerySnapshot =
+            await getSpecNameVersionFromFirestore(name, version);
+        const dependency: object | null =
+            pickSpecNameVersion(querySnapshot);
+
+        res.status(200).send(dependency === null ? "null" : dependency);
     }
+
     async orgDeps(req: Request, res: Response): Promise<any> {
         const org = req.params.org;
         const name = org + "/" + req.params.name;
         const version = req.params.version;
 
-        const querySnapshot = await getSpecNameVersion(name, version);
-        querySnapshot.forEach((doc) => {
-            const packageInfo = doc.data();
-            let deps = packageInfo.deps;
-            if (deps) {
-                let dep = {};
-                let i: number = 0;
-                for (let key in deps) {
-                    if (deps[key]["src"] === "poac") {
-                        deps[key] = deps[key]["version"];
-                    }
-                    else if (deps[key]["src"] === "github") {
-                        deps[key] = deps[key]["tag"];
-                    }
-                    dep[i.toString()] = {
-                        "name": key,
-                        "version": deps[key]
-                    };
-                    ++i;
-                }
-                res.status(200).send(dep);
-            } else {
-                res.status(200).send("null");
-            }
-        });
+        const querySnapshot: FirebaseFirestore.QuerySnapshot =
+            await getSpecNameVersionFromFirestore(name, version);
+        const dependency: object | null =
+            pickSpecNameVersion(querySnapshot);
+
+        res.status(200).send(dependency === null ? "null" : dependency);
     }
 
     async versions(req: Request, res: Response): Promise<any> {
@@ -129,7 +129,7 @@ class Controller {
         const name = req.params.name;
         const version = req.params.version;
 
-        const querySnapshot = await getSpecNameVersion(name, version);
+        const querySnapshot = await getSpecNameVersionFromFirestore(name, version);
         // Ref: https://stackoverflow.com/questions/14774907/typescript-convert-a-bool-to-string-value
         const restr: string = <string><any>(!querySnapshot.empty);
         res.status(200).send(restr);
@@ -139,7 +139,7 @@ class Controller {
         const name = org + "/" + req.params.name;
         const version = req.params.version;
 
-        const querySnapshot = await getSpecNameVersion(name, version);
+        const querySnapshot = await getSpecNameVersionFromFirestore(name, version);
         const restr: string = <string><any>(!querySnapshot.empty);
         res.status(200).send(restr);
     }
