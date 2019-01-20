86c66ba728284b88634030137375b110dcab9130
matken11235 <matken11235@gmail.com>
2019-01-21 06:04:54 +0900

    Fix hosting/webpack.config.js

--- hosting/webpack.config.js
+++ hosting/webpack.config.js
@@ -90,9 +90,7 @@ module.exports = {
                 test: /\.elm$/,
                 exclude: [/elm-stuff/, /node_modules/],
                 loader: "elm-webpack-loader",
-                options: {
-                    optimize: true
-                }
+                options: {}
             },
         ],
         noParse: [/\.elm$/]
