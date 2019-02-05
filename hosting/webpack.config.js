f40412d55f2569e6584fb8fb2a6740469317befe
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-02-06 07:01:27 +0900

    To switch optimize to true in webpack.config.js

--- hosting/webpack.config.js
+++ hosting/webpack.config.js
@@ -90,7 +90,9 @@ module.exports = {
                 test: /\.elm$/,
                 exclude: [/elm-stuff/, /node_modules/],
                 loader: "elm-webpack-loader",
-                options: {}
+                options: {
+                    optimize: true
+                }
             },
         ],
         noParse: [/\.elm$/]
