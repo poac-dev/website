efa3534ae3687b35cf9312c766ef7da0a2b111a9
matken11235 <matken11235@gmail.com>
2019-02-06 03:47:44 +0900

    Update elm/Update.elm

--- hosting/elm/Update.elm
+++ hosting/elm/Update.elm
@@ -3,8 +3,8 @@ module Update exposing (update, urlUpdate)
 import Messages exposing (..)
 import Model exposing (..)
 import Navigation as Nav
-import Routing exposing (Route(..), parse, toPath)
 import Ports
+import Routing exposing (Route(..), parse, toPath)
 import Scroll
 
 
@@ -16,15 +16,15 @@ update msg model =
                 currentRoute =
                     parse location
             in
-                urlUpdate { model | route = currentRoute }
+            urlUpdate { model | route = currentRoute }
 
         NavigateTo route ->
             ( model, Nav.newUrl <| toPath route )
 
         -- HandleInput id value ->
-
         HandleSearchInput value ->
             ( { model | search = value }, Cmd.none )
+
         HandleTokenInput value ->
             ( { model | newTokenName = value }, Cmd.none )
 
@@ -32,40 +32,48 @@ update msg model =
             case user of
                 Just u ->
                     ( { model | otherUser = Success u }, Cmd.none )
+
                 Nothing ->
                     ( { model | otherUser = Failure }, Cmd.none )
 
         FetchToken token ->
             ( { model | currentToken = Success token }, Cmd.none )
-        CreateToken -> -- TODO: refresh model.newTokenName
+
+        CreateToken ->
+            -- TODO: refresh model.newTokenName
             if String.isEmpty model.newTokenName then
                 ( model, Cmd.none )
+
             else
                 ( { model | currentToken = Requesting }
                 , Cmd.batch
-                      [ Ports.createToken model.newTokenName
-                      , Ports.fetchToken ()
-                      ]
+                    [ Ports.createToken model.newTokenName
+                    , Ports.fetchToken ()
+                    ]
                 )
+
         RevokeToken id ->
             ( { model | currentToken = Requesting }
             , Cmd.batch
-                  [ Ports.deleteToken id
-                  , Ports.fetchToken ()
-                  ]
+                [ Ports.deleteToken id
+                , Ports.fetchToken ()
+                ]
             )
 
         DeletePackage name version ->
             ( model
-            , Ports.deletePackage (name, version)
+            , Ports.deletePackage ( name, version )
             )
 
         LoginOrSignup ->
             ( model, Ports.signin () )
+
         Signin (Just user) ->
             ( { model | signinUser = Success user }, Cmd.none )
+
         Signin Nothing ->
             ( { model | signinUser = Requesting }, Cmd.none )
+
         Signout ->
             ( model
             , Cmd.batch
@@ -77,6 +85,7 @@ update msg model =
 
         FetchSigninId (Just signinId) ->
             ( { model | signinId = signinId }, Cmd.none )
+
         FetchSigninId Nothing ->
             ( { model | signinId = "" }, Cmd.none )
 
@@ -85,6 +94,7 @@ update msg model =
 
         FetchDetailedPackage (Just packages) ->
             ( { model | detailedPackage = Success packages }, Cmd.none )
+
         FetchDetailedPackage Nothing ->
             ( { model | detailedPackage = Failure }, Cmd.none )
 
@@ -97,7 +107,9 @@ update msg model =
                         , update (Fadein Section1)
                             |> Scroll.onCrossDown 600
                         ]
-                        move model
+                        move
+                        model
+
                 _ ->
                     ( model, Cmd.none )
 
@@ -110,20 +122,25 @@ update msg model =
                     case view of
                         GetStart ->
                             asGetStartIn
+
                         Section1 ->
                             asSection1In
+
                 newModel =
                     True
-                    |> asIn model.isFadein
-                    |> asIsFadein model
+                        |> asIn model.isFadein
+                        |> asIsFadein model
             in
-                ( newModel, Cmd.none )
+            ( newModel, Cmd.none )
 
         OnSearchInput searchInput ->
             ( { model | searchInput = searchInput }, Cmd.none )
+
         Search key ->
-            if key == 13 then -- Enter key
+            if key == 13 then
+                -- Enter key
                 ( model, Nav.newUrl <| toPath <| PackagesRoute "" )
+
             else
                 ( model, Cmd.none )
 
@@ -131,27 +148,34 @@ update msg model =
             ( { model | isChecked = bool }, Cmd.none )
 
 
-
 setSection1 : Bool -> IsFadein -> IsFadein
 setSection1 newBool isFadein =
     { isFadein | section1 = newBool }
+
+
 asSection1In : IsFadein -> Bool -> IsFadein
 asSection1In =
-    flip setSection1
+    \b a -> setSection1 a b
+
 
 setGetStart : Bool -> IsFadein -> IsFadein
 setGetStart newBool isFadein =
     { isFadein | getStart = newBool }
+
+
 asGetStartIn : IsFadein -> Bool -> IsFadein
 asGetStartIn =
-    flip setGetStart
+    \b a -> setGetStart a b
+
 
 setIsFadein : IsFadein -> Model -> Model
 setIsFadein newIsFadein model =
     { model | isFadein = newIsFadein }
+
+
 asIsFadein : Model -> IsFadein -> Model
 asIsFadein =
-    flip setIsFadein
+    \b a -> setIsFadein a b
 
 
 getUserId : RemoteData User -> Maybe String
@@ -159,6 +183,7 @@ getUserId user =
     case user of
         Success u ->
             Just u.id
+
         _ ->
             Nothing
 
@@ -173,67 +198,84 @@ urlUpdate model =
             case model.otherUser of
                 NotRequested ->
                     ( { model | otherUser = Requesting }
-                    , Cmd.batch [ Ports.fetchUser id
-                                , Ports.fetchOwnedPackages id
-                                ]
+                    , Cmd.batch
+                        [ Ports.fetchUser id
+                        , Ports.fetchOwnedPackages id
+                        ]
                     )
+
                 Success user ->
                     if user.id /= id then
                         ( { model | otherUser = Requesting }
-                        , Cmd.batch [ Ports.fetchUser id
-                                    , Ports.fetchOwnedPackages id
-                                    ]
+                        , Cmd.batch
+                            [ Ports.fetchUser id
+                            , Ports.fetchOwnedPackages id
+                            ]
                         )
+
                     else
                         ( model, Cmd.none )
+
                 _ ->
                     ( model, Cmd.none )
 
         SettingsRoute mode ->
             case mode of
                 "tokens" ->
-                    case (model.signinUser, model.currentToken) of
-                        (Success user, NotRequested) ->
+                    case ( model.signinUser, model.currentToken ) of
+                        ( Success user, NotRequested ) ->
                             ( { model | currentToken = Requesting }
                             , Ports.fetchToken ()
                             )
+
                         _ ->
                             ( model, Cmd.none )
+
                 "packages" ->
                     case model.signinUser of
                         Success _ ->
                             ( { model | listPackages = Requesting }
                             , Ports.fetchSigninUserId ()
                             )
+
                         _ ->
                             ( model, Cmd.none )
-                _ ->
-                    ( model, Cmd.none )
-        SettingRoute -> -- /settings だと、/settings/tokenと同じだから
-            case (model.signinUser, model.currentToken) of
-                (Success user, NotRequested) ->
-                    ( { model | currentToken = Requesting }
-                    , Ports.fetchToken ()
-                    )
+
                 _ ->
                     ( model, Cmd.none )
 
-        PackagesRoute name -> -- TODO: 新規アクセスの度に，listPackagesを空に？？？Usersにアクセスした後だとおかしくなる．
+        SettingRoute ->
+            -- /settings だと、/settings/tokenと同じだから
+            case ( model.signinUser, model.currentToken ) of
+                ( Success user, NotRequested ) ->
+                    ( { model | currentToken = Requesting }
+                    , Ports.fetchToken ()
+                    )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        PackagesRoute name ->
+            -- TODO: 新規アクセスの度に，listPackagesを空に？？？Usersにアクセスした後だとおかしくなる．
             if String.isEmpty name then
                 ( model, Ports.instantsearch () )
+
             else
                 case model.detailedPackage of
                     NotRequested ->
                         ( { model | detailedPackage = Requesting }
                         , Ports.fetchDetailedPackage name
                         )
+
                     Success detailedPackage ->
                         if detailedPackage.name /= name then
                             ( { model | detailedPackage = Requesting }
                             , Ports.fetchDetailedPackage name
                             )
+
                         else
                             ( model, Cmd.none )
+
                     _ ->
                         ( model, Cmd.none )
 
@@ -242,29 +284,34 @@ urlUpdate model =
                 oname =
                     org ++ "/" ++ name
             in
-                if String.isEmpty oname then
-                    case model.listPackages of
-                        NotRequested ->
-                            ( { model | listPackages = Requesting }
-                            , Ports.fetchPackages ()
-                            )
-                        _ ->
-                            ( model, Cmd.none )
-                else
-                    case model.detailedPackage of
-                        NotRequested ->
+            if String.isEmpty oname then
+                case model.listPackages of
+                    NotRequested ->
+                        ( { model | listPackages = Requesting }
+                        , Ports.fetchPackages ()
+                        )
+
+                    _ ->
+                        ( model, Cmd.none )
+
+            else
+                case model.detailedPackage of
+                    NotRequested ->
+                        ( { model | detailedPackage = Requesting }
+                        , Ports.fetchDetailedPackage oname
+                        )
+
+                    Success detailedPackage ->
+                        if detailedPackage.name /= oname then
                             ( { model | detailedPackage = Requesting }
                             , Ports.fetchDetailedPackage oname
                             )
-                        Success detailedPackage ->
-                            if detailedPackage.name /= oname then
-                                ( { model | detailedPackage = Requesting }
-                                , Ports.fetchDetailedPackage oname
-                                )
-                            else
-                                ( model, Cmd.none )
-                        _ ->
+
+                        else
                             ( model, Cmd.none )
+
+                    _ ->
+                        ( model, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
