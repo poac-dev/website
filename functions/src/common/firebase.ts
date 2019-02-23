88b4d370619533c849014f219a802b46397b12c8
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-02-23 12:46:02 +0900

    Remove timestampsInSnapshots setting

--- functions/src/common/firebase.ts
+++ functions/src/common/firebase.ts
@@ -3,9 +3,6 @@ import * as functions from 'firebase-functions';
 
 admin.initializeApp(functions.config().firebase);
 const firestore = admin.firestore();
-firestore.settings({
-    timestampsInSnapshots: true
-});
 const storage = admin.storage();
 
 export { admin, firestore, functions, storage };
