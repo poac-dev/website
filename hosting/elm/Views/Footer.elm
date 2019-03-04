94f4495ad0a435b4ea5c634c88ebc6841cf88508
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-03-05 01:20:42 +0900

    Add status link to footer

--- hosting/elm/Views/Footer.elm
+++ hosting/elm/Views/Footer.elm
@@ -11,16 +11,18 @@ view =
         [ hr [ class "border-hr" ] []
         , div [ class "footer-content" ]
             [ div [ class "links" ]
-                --              [ a [ href ("/" ++ (String.toLower "Policies")) ]
-                --                  [ text "Policies"
-                --                  ]
-                --              , a [ href ("/" ++ (String.toLower "Sponsors")) ]
-                --                  [ text "Sponsors"
-                --                  ]
+--              [ a [ href ("/" ++ (String.toLower "Policies")) ]
+--                  [ text "Policies"
+--                  ]
+--              , a [ href ("/" ++ (String.toLower "Sponsors")) ]
+--                  [ text "Sponsors"
+--                  ]
                 [ a [ href "mailto:support@poac.io?subject=[Feedback]" ]
                     [ text "Feedback" ]
                 , a [ href "https://github.com/poacpm" ]
                     [ text "GitHub" ]
+                , a [ href "https://status.poac.pm" ]
+                    [ text "Status" ]
                 ]
             , div [ class "copyright" ]
                 [ text "©︎ 2018 "
