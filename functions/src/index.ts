651cbbcca26fbcac1e341a551095c1665717163d
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-03-16 20:32:05 +0900

    Remove API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,7 @@
-import app from "./common/server";
 import handler from "./handlers/handler";
-import { functions } from "./common/firebase";
 
 // This line is important. What we are doing here
 //  is exporting ONE function with the name
 //  `api` which will always route
-exports.api = functions.https.onRequest(app);
 exports.onMetadataCreated = handler.onMetadataCreated;
 exports.onMetadataDeleted = handler.onMetadataDeleted;
