b104ada4979cfe7f78d34d47e4aa6ee218f1dc61
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-07-22 06:09:23 +0900

    Fix functions/src/handlers/handler.ts

--- functions/src/common/firebase.ts
+++ functions/src/common/firebase.ts
@@ -3,6 +3,5 @@ import * as functions from 'firebase-functions';
 
 admin.initializeApp(functions.config().firebase);
 const firestore = admin.firestore();
-const storage = admin.storage();
 
-export { admin, firestore, functions, storage };
+export { admin, firestore, functions };
