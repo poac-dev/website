79df7818f86185f997a8a6730d5cd7addf6dd954
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-09-05 02:26:40 +0900

    Remove unused statements

--- elm/Model.elm
+++ elm/Model.elm
@@ -1,52 +1,12 @@
 module Model exposing (..)
 
 import Browser.Navigation exposing (Key)
-import Dict exposing (Dict)
 import Route exposing (Route)
-
-
-type RemoteData a
-    = NotRequested
-    | Requesting
-    | Success a
-    | Failure
-
 
 
 type alias Flags =
     { api : String
     }
-
-
-type alias PackageMetadata =
-    { cppVersion : Int
-    , owner : String
-    , repo : String
-    , version : String
-    , description : String
-    , dependencies : Maybe (Dict String String)
-    , devDependencies : Maybe (Dict String String)
-    , buildDependencies : Maybe (Dict String String)
-    , build : Maybe BuildMetadata
-    , test : Maybe TestMetadata
-    , packageType : String
-    , commitSha : String
-    , readme : String
-    }
-
-
-type alias BuildMetadata =
-   { system : Maybe String
-   , bin : Maybe Bool
-   , lib : Maybe Bool
-   , compileArgs : Maybe (List String)
-   , linkArgs : Maybe (List String)
-   }
-
-
-type alias TestMetadata =
-   { framework : Maybe String
-   }
 
 
 type alias IsFadein =
@@ -61,9 +21,6 @@ type alias Model =
     { flags : Flags
     , navKey : Key
     , route : Route
-    , ownPackages : RemoteData (List (RemoteData PackageMetadata))
-    , packageVersions : RemoteData (List String)
-    , package : RemoteData PackageMetadata
     , search : String
     , isFadein : IsFadein
     , searchInput : String
