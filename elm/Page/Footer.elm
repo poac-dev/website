404ec96c74a622ae418ed7de0caaf99709ff151e
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-09-06 11:40:24 +0900

    Remove elm/Page/FooterCss.elm

--- elm/Page/Footer.elm
+++ elm/Page/Footer.elm
@@ -1,11 +1,54 @@
 module Page.Footer exposing (view)
 
-import Page.FooterCss exposing (divider90, links, aFooter)
+import Css exposing (..)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css, href)
 import Messages exposing (..)
 import Route
 import Model exposing (Model)
+
+
+
+divider90 : Style
+divider90 =
+    Css.batch
+        [ width (90 |> pct)
+        , marginTop (80 |> px)
+        , display block
+        , border (0 |> px)
+        , height (0 |> px)
+        , borderTop3 (1 |> px) solid (rgba 0 0 0 0.1)
+        , borderBottom3 (1 |> px) solid (rgba 255 255 255 0.3)
+        ]
+
+
+links : Model -> Style
+links model =
+    Css.batch
+        [ width (80 |> vw)
+        , marginTop (40 |> px)
+        , marginLeft auto
+        , marginRight auto
+        , paddingBottom (125 |> px)
+        , paddingTop (18 |> px)
+        , displayFlex
+        , justifyContent spaceAround
+        , flexWrap wrap
+        , alignItems center
+        , property "align-content" "space-around"
+        , fontWeight (int 900)
+        , letterSpacing (1.25 |> px)
+        , fontSize ((if model.width < 1000 then 0.8 else 0.6) |> rem)
+        , lineHeight (12 |> px)
+        ]
+
+
+aFooter : List (Attribute msg) -> List (Html msg) -> Html msg
+aFooter =
+    styled a
+        [ textDecoration none
+        , padding (20 |> px)
+        ]
 
 
 view : Model -> Html Msg
