1a65a3b1ca84279d2070b90fa4e34b21654506ea
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-15 21:50:38 +0900

    Implement search component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
 import { Flex, IconButton, useColorMode, useColorModeValue, Spacer, Image, LinkBox, LinkOverlay } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/router";
 
 import UserMenu from "./UserMenu";
+import Search from "./Search";
 
 export default function Header(): JSX.Element {
+    const router = useRouter();
     const { toggleColorMode } = useColorMode();
     const colorModeIcon = useColorModeValue(<MoonIcon />, <SunIcon />);
 
@@ -15,7 +18,7 @@ export default function Header(): JSX.Element {
                     <Image width="80px" src="/logo.svg" alt="Icon" />
                 </LinkOverlay>
             </LinkBox>
-            <Spacer />
+            {router.pathname === "/" ? <Spacer /> : <Search />}
             <IconButton
                 onClick={toggleColorMode}
                 aria-label="Toggle theme"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
 import type { NextPage } from "next";
 import { VStack, Heading } from "@chakra-ui/react";
 
+import Search from "../components/Search";
 
 const Home: NextPage = () => {
     return (
         <VStack spacing={5}>
             <Heading as="h1">
-            Package Manager for C++ Developers
+                Package Manager for C++ Developers
             </Heading>
             <Heading as="h2" size="md">
-            Poac is a C++ package manager which is for open source software.
-                <br/>
-            Easy to introduce to your projects; you can use packages intuitively.
+                Poac is a C++ package manager which is for open source software.<br/>
+                Easy to introduce to your projects; you can use packages intuitively.
             </Heading>
+            <Search />
         </VStack>
     );
 };
