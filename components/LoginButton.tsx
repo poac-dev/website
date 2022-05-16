7abbbc3344323a64e2ff2879ff4da416d88d5e98
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-16 21:03:33 +0900

    Improve LoginButton

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -2,13 +2,21 @@ import { LockIcon } from "@chakra-ui/icons";
 import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
 import { Button } from "@chakra-ui/react";
 import { useCallback } from "react";
+import { useRouter } from "next/router";
 
 export default function LoginButton(): JSX.Element {
+    const router = useRouter();
+    
     const signIn = useCallback((): void => {
         supabaseClient.auth.signIn(
             { provider: "github" },
+            {
+                redirectTo: (process.env.NODE_ENV === "development" ?
+                    "http://localhost:3000" :
+                    "https://poac.pm") + router.asPath,
+            }
         );
-    }, []);
+    }, [router.asPath]);
 
     return (
         <Button
