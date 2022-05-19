aac0dba1f0f5e9c99cb6ac5bfba1703647e73afe
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-19 11:58:53 +0900

    Remove cookieOptions

--- pages/api/auth/[...supabase].ts
+++ pages/api/auth/[...supabase].ts
@@ -2,5 +2,4 @@ import { handleAuth } from "@supabase/supabase-auth-helpers/nextjs";
 
 export default handleAuth({
     logout: { returnTo: "/" },
-    cookieOptions: { lifetime: 1 * 365 * 24 * 60 * 60 }, // Keep the user logged in for a year.
 });
