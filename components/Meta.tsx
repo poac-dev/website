f0040f8e968765978368826856f2378595dde76c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-20 09:55:13 +0900

    Use Cloudinary for dynamic OGP images instead of using Next.js APIs

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,8 +1,25 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import truncate from "truncate";
 
-import { BASE_URL } from "~/utils/constants";
 import type { Package } from "~/utils/types";
+
+function constructOgImageUrl(name: string, version: string, description: string): string {
+    const ogImageUrlLeft = "https://res.cloudinary.com/poac/image/upload";
+    const ogImageUrlRight = "opg_bg.png";
+
+    const cldFit = "w_1000,c_fit";
+    const cldFont = "l_text:Roboto";
+    const cldHeading = `${cldFont}_70_bold_center`;
+    const cldText = `${cldFont}_35_center`;
+    const cldRelativeBottom = (y: number): string => `fl_layer_apply,y_${y}_add_$lineHeight`;
+
+    const ogImageName = `${cldFit},${cldHeading}:${name.replace("/", "%252F")},$lineHeight_h`;
+    const ogImageVersion = `${cldFit},${cldText}:(${version !== "latest" ? "v" : ""}${version})/${cldRelativeBottom(10)}`;
+    const ogImageDesc = `${cldFit},${cldText}:${truncate(description, 180)}/${cldRelativeBottom(70)}`;
+
+    return `${ogImageUrlLeft}/${ogImageName}/${ogImageVersion}/${ogImageDesc}/${ogImageUrlRight}`;
+}
 
 interface MetaProps {
     title?: string;
@@ -16,7 +33,7 @@ export default function Meta(props: MetaProps): JSX.Element {
     const title = props.title ? `${props.title} - Poac`: "Poac: Package Manager for C++";
     const description = props.description ?? "Package Manager for C++.";
     const ogImagePath = props.package && props.description ?
-        `${BASE_URL}/api/ogp?name=${props.package.name}&ver=${props.package.version}&desc=${description}` :
+        constructOgImageUrl(props.package.name, props.package.version, props.description) :
         "https://poac.pm/icon-512x512.png";
 
     return (
@@ -31,7 +48,7 @@ export default function Meta(props: MetaProps): JSX.Element {
             <meta property="description" content={description} />
             <meta property="og:description" content={description} />
             <meta property="og:image" content={ogImagePath} />
-            <meta property="og:site_name" content="Poac.pm" />
+            <meta property="og:site_name" content="poac.pm" />
             <meta property="og:type" content="website" />
 
             <meta name="theme-color" content="#000000" />
