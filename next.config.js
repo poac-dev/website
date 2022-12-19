b6928a111f437dcb09e6566bfc0c4dd16315ed17
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-12-18 21:56:03 -0800

    Add some security headers (#763)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,32 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
+const securityHeaders = [
+    {
+        key: "X-Frame-Options",
+        value: "deny",
+    },
+    {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+    },
+    {
+        key: "Referrer-Policy",
+        value: "strict-origin-when-cross-origin",
+    },
+];
+
 const nextConfig = {
     reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: "/:path*",
+                headers: securityHeaders,
+            },
+        ];
+    },
     webpack: config => {
         config.resolve.alias["~"] = path.resolve(__dirname);
         return config;
