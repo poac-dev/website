dac7aeecdd07e70c387814c15a5602e52a878c76
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-08 02:06:40 -0700

    Add privacy-friendly Vercel Web Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,22 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import NextNProgress from "nextjs-progressbar";
+import { Analytics } from "@vercel/analytics/react";
 
 import Layout from "~/components/Layout";
 import theme from "~/utils/theme";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
-        <ChakraProvider theme={theme}>
-            <NextNProgress options={{ showSpinner: false }} />
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </ChakraProvider>
+        <>
+            <ChakraProvider theme={theme}>
+                <NextNProgress options={{ showSpinner: false }} />
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ChakraProvider>
+            <Analytics />
+        </>
     );
 }
 
