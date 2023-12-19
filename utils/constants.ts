fd2fd1b8bdc695732887d0e1153b7b97ee752ad7
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-12-18 23:34:40 -0800

    Update API URL

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,7 +3,7 @@ export const BASE_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:3000"
         : "https://poac.dev";
-export const BASE_API_URL = "https://api.poac.dev/v1";
+export const BASE_API_URL = "https://poac-api.shuttleapp.rs/v1";
 // process.env.NODE_ENV === "development"
 //     ? "http://127.0.0.1:8000/v1"
-//     : "https://api.poac.dev/v1";
+//     : "https://poac-api.shuttleapp.rs/v1";
