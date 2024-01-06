dd7983f553c72db8e6f3176eb03329d6e8a88301
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-01-05 19:31:33 -0800

    docs.poac.dev is now up

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
                                     isExternal
                                     as={Link}
                                     className="mx-0 my-4 md:mx-4 md:my-0 w-full md:w-auto"
-                                    href="https://poac.mintlify.app/guides"
+                                    href="https://docs.poac.dev/guides"
                                     radius="full"
                                     size="lg"
                                     endContent={
