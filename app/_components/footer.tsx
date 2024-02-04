d31784eb1c25fa72b734e371292529f8181ab028
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-02-04 01:02:49 -0800

    Link copyright

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,19 @@
+import { Link } from "@nextui-org/react";
+
 export function Footer() {
     return (
         <footer className="container mx-auto max-w-7xl pb-12 px-12">
             <div className="flex flex-col justify-center items-center gap-1">
                 <p className="text-sm text-default-400">
-                    © {new Date().getFullYear()} Ken Matsui
+                    © {new Date().getFullYear()}{" "}
+                    <Link
+                        isExternal
+                        aria-label="Ken Matsui"
+                        className="text-sm text-default-400"
+                        href="https://github.com/ken-matsui"
+                    >
+                        Ken Matsui
+                    </Link>
                 </p>
             </div>
         </footer>
